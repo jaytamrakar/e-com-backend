34715986f806c01b661348beb0efeb33e626031a
jaytamrakar <jaytamrakar5@gmail.com>
2023-07-27 03:14:29 +0530

    orders and product for admin

--- controller/Order.js
+++ controller/Order.js
@@ -1,9 +1,9 @@
 const { Order } = require("../model/Order");
 
 exports.fetchOrdersByUser = async (req, res) => {
-  const { user } = req.query;
+  const { userId } = req.params;
   try {
-    const orders = await Order.find({ user: user });
+    const orders = await Order.find({ user: userId });
     res.status(200).json(orders);
   } catch (error) {
     res.status(404).json(error);
@@ -39,6 +39,38 @@ exports.updateOrder = async (req, res) => {
       new: true,
     });
     res.status(200).json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+};
+
+exports.fetchAllOrders = async (req, res) => {
+  let query = Order.find({ deleted: { $ne: true } });
+  let totalOrdersQuery = Order.find({ deleted: { $ne: true } });
+
+  const totalDocs = await totalOrdersQuery.count().exec();
+
+  if (req.query._sort && req.query._order) {
+    query = query.sort({ [req.query._sort]: req.query._order });
+    totalOrdersQuery = totalOrdersQuery.sort({
+      [req.query._sort]: req.query._order,
+    });
+  }
+
+  if (req.query._page && req.query._limit) {
+    const pageSize = req.query._limit;
+    const page = req.query._page;
+    query = query.skip(pageSize * (page - 1)).limit(pageSize);
+    totalOrdersQuery = totalOrdersQuery
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+  }
+
+  try {
+    const docs = await query.exec();
+    res.set("X-Total-Count", totalDocs);
+    res.status(200).json(docs);
   } catch (error) {
     console.error(error);
     res.status(400).json(error);
